453108e46ea033fcb02424c513134144d6eacad3
Paul Kohlhoff <paul.kohlhoff@gmail.com>
2018-04-22 18:07:31 -0600

    adding s3 file upload

--- workers/test_export_old.js
+++ workers/test_export_old.js
@@ -5,6 +5,8 @@ var publisher = require('../publisher');
 var moment = require('moment');
 var sleep = require('sleep');
 var xlsx = require('xlsx');
+var AWS = require('aws-sdk');
+var s3Stream = require('s3-upload-stream')(new AWS.S3());
 const uuid = require('uuid/v4');
 const mongo_connectionString = require('../db').mongo_connectionString;
 const rmq_connectionString = require('../env').rmq_connectionString;
@@ -21,13 +23,15 @@ if (!process.argv.length) sleep.sleep(30);
     // Database Name
   const dbName = 'test';
 
+  AWS.config.loadFromPath('./config.json');
+
   // Use connect method to connect to the server
   MongoClient.connect(mongo_connectionString + '/' + dbName, async function(err, client) {
     assert.equal(null, err);
     console.log("Connected successfully to Mongo server: " + mongo_connectionString + '/' + dbName);
 
         var db = client.db(dbName);
-        const id_submission = "089b4290-e24d-4d77-9633-6b0c744a81ab";
+        const id_submission = "a37649b4-5a54-4874-928f-01210fd8008b";
 
         let cursor = db.collection(c).find({"id_submission" : id_submission});
         var responses = await cursor.toArray();
@@ -41,6 +45,22 @@ if (!process.argv.length) sleep.sleep(30);
         xlsx.utils.book_append_sheet(wb, ws, 'Responses');
 
         xlsx.writeFile(wb, `/tmp/${id_submission}.xlsx`);
+
+        var read = fs.createReadStream(`/tmp/${id_submission}.xlsx`);
+        var upload = s3Stream.upload({
+          "Bucket": "gamesense-test-responses",
+          "Key": `${id_submission}.xlsx`
+        });
+
+        upload.on('error', function (error) {
+          console.log(error);
+        });
+         
+        upload.on('part', function (details) {
+          console.log(details);
+        });
+
+        read.pipe(upload);
 
         client.close();
 
