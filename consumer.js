17dc2a72b4fb505c1c3954f8e1dd9b13db3d7492
Paul Kohlhoff <paul.kohlhoff@gmail.com>
2018-04-22 18:07:01 -0600

    made debugs and errors go to log for any consumer

--- consumer.js
+++ consumer.js
@@ -4,12 +4,8 @@ var fs = require('fs');
 var util = require('util');
 var sleep = require('sleep');
 var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
+var error_file = fs.createWriteStream(__dirname + '/error.log', {flags : 'w'});
 var log_stdout = process.stdout;
-
-console.log = function(d) { //
-  log_file.write(util.format(d) + '\n');
-  log_stdout.write(util.format(d) + '\n');
-};
 
 
 
@@ -23,6 +19,18 @@ const uuidForCurrentExecution = uuid();
 //		@method publish
 //		@method close
 function MakeConsumer(func, q, connectionString) {
+
+	// enable debug logging
+	console.log = function(d) { //
+	  log_file.write(util.format(d) + '\n');
+	  log_stdout.write(util.format(d) + '\n');
+	};
+
+	// enable error logging
+	console.error = function(d) { //
+	  error_file.write(util.format(d) + '\n');
+	  log_stdout.write(util.format(d) + '\n');
+	};
 
   	return function() {
 
@@ -67,5 +75,5 @@ function consume(receive, q, connectionString) {
 
 module.exports.MakeConsumer = MakeConsumer;
 module.exports.consume = consume;
-module.exports.uuidForCurrentExecution = uuidForCurrentExecution
+module.exports.uuidForCurrentExecution = uuidForCurrentExecution;
 	     
