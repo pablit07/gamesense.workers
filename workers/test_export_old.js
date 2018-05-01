47c59dce82c41046650666c2114248514946caef
Paul Kohlhoff <paul.kohlhoff@gmail.com>
2018-05-01 05:19:13 -0600

    finishing player spreadsheet

--- workers/test_export_old.js
+++ workers/test_export_old.js
@@ -7,6 +7,7 @@ var sleep = require('sleep');
 var xlsx = require('xlsx');
 var AWS = require('aws-sdk');
 var s3Stream = require('s3-upload-stream')(new AWS.S3());
+var fs = require('fs');
 const uuid = require('uuid/v4');
 const mongo_connectionString = require('../db').mongo_connectionString;
 const rmq_connectionString = require('../env').rmq_connectionString;
@@ -21,9 +22,7 @@ if (!process.argv.length) sleep.sleep(30);
 // try {
 
     // Database Name
-  const dbName = 'test';
-
-  AWS.config.loadFromPath('./config.json');
+  const dbName = 'prod';
 
   // Use connect method to connect to the server
   MongoClient.connect(mongo_connectionString + '/' + dbName, async function(err, client) {
@@ -31,16 +30,106 @@ if (!process.argv.length) sleep.sleep(30);
     console.log("Connected successfully to Mongo server: " + mongo_connectionString + '/' + dbName);
 
         var db = client.db(dbName);
-        const id_submission = "a37649b4-5a54-4874-928f-01210fd8008b";
-
-        let cursor = db.collection(c).find({"id_submission" : id_submission});
-        var responses = await cursor.toArray();
-
         var wb = xlsx.utils.book_new();
+        const id_submission = "a37649b4-5a54-4874-928f-01210fd8008b";
+        const header = {
+            'time_video_started_formatted': 1,
+            'pitch': 1,
+            'response_location_name': 1,
+            'time_answered_formatted': 1,
+            'test_id': 1,
+            'response_name': 1,
+            'player_id': 1,
+            'occlusion': 1,
+            'type_score': 1,
+            'location_score': 1,
+            'completely_correct_score': 1
+        };
+        const header_plus_2 = {
+            'occlusion_plus_2_type_avg': 1,
+            'occlusion_plus_2_location_avg': 1,
+            'occlusion_plus_2_completely_correct_avg': 1
+        };
+        const header_plus_5 = {
+            'occlusion_plus_5_type_avg': 1,
+            'occlusion_plus_5_location_avg': 1,
+            'occlusion_plus_5_completely_correct_avg': 1
+        };
+        const headerKeys = Object.keys(Object.assign({}, header, header_plus_2));
 
-        var ws = xlsx.utils.json_to_sheet(responses);
+        var cursor = db.collection(c).find({"id_submission" : id_submission, "occlusion" : 'R+2'});
+        cursor.project(Object.assign({}, header, header_plus_2));
 
-        // xlsx.utils.sheet_add_aoa(ws, [['','','','','','','','','','','',responses[0].occlusion_none_type_avg,responses[0].occlusion_none_location_avg,responses[0].occlusion_none_completely_correct_avg]], {});
+        var responses = await cursor.toArray(),
+            occlusion_plus_2_type_avg = responses[0].occlusion_plus_2_type_avg,
+            occlusion_plus_2_location_avg = responses[0].occlusion_plus_2_location_avg,
+            occlusion_plus_2_completely_correct_avg = responses[0].occlusion_plus_2_completely_correct_avg
+        // clear out columns for writing to sheet
+        responses.forEach((r) => { 
+            delete r._id;
+            r.occlusion_plus_2_type_avg = null;
+            r.occlusion_plus_2_location_avg = null;
+            r.occlusion_plus_2_completely_correct_avg = null;
+        });
+
+        var ws = xlsx.utils.json_to_sheet(responses, {header: headerKeys});
+
+        xlsx.utils.sheet_add_aoa(ws, [['','','','','','','','','','','',
+            occlusion_plus_2_type_avg,
+            occlusion_plus_2_location_avg,
+            occlusion_plus_2_completely_correct_avg,
+            Math.round(occlusion_plus_2_type_avg * 1000),
+            Math.round(occlusion_plus_2_location_avg * 1000),
+            Math.round(occlusion_plus_2_completely_correct_avg * 1000)
+        ]], {origin:-1});
+
+        cursor = db.collection(c).find({"id_submission" : id_submission, "occlusion" : 'R+5'});
+        cursor.project(Object.assign({}, header, header_plus_5));
+
+        responses = await cursor.toArray();
+        let occlusion_plus_5_type_avg = responses[0].occlusion_plus_5_type_avg,
+            occlusion_plus_5_location_avg = responses[0].occlusion_plus_5_location_avg,
+            occlusion_plus_5_completely_correct_avg = responses[0].occlusion_plus_5_completely_correct_avg;
+        // clear out columns for writing to sheet
+        responses.forEach((r) => { 
+            delete r._id;
+            r.occlusion_plus_5_type_avg = null;
+            r.occlusion_plus_5_location_avg = null;
+            r.occlusion_plus_5_completely_correct_avg = null;
+        });
+
+        xlsx.utils.sheet_add_json(ws, responses, {header: headerKeys, skipHeader: true, origin: -1});
+
+        xlsx.utils.sheet_add_aoa(ws, [['','','','','','','','','','','',
+            occlusion_plus_5_type_avg,
+            occlusion_plus_5_location_avg,
+            occlusion_plus_5_completely_correct_avg,
+            Math.round(occlusion_plus_5_type_avg * 1000),
+            Math.round(occlusion_plus_5_location_avg * 1000),
+            Math.round(occlusion_plus_5_completely_correct_avg * 1000)
+        ]], {origin:-1});
+
+        // average of +2 and +5
+        xlsx.utils.sheet_add_aoa(ws, [['','','','','','','','','','','','','','',
+            Math.round((occlusion_plus_2_type_avg + occlusion_plus_5_type_avg) / 2),
+            Math.round((occlusion_plus_2_location_avg + occlusion_plus_5_location_avg) / 2),
+            Math.round((occlusion_plus_2_completely_correct_avg + occlusion_plus_5_completely_correct_avg) / 2)
+        ]], {origin:-1});
+
+        // "none" results
+        cursor = db.collection(c).find({"id_submission" : id_submission, "occlusion" : 'None'});
+        cursor.project(header);
+
+        responses = await cursor.toArray();
+        // let occlusion_plus_5_type_avg = responses[0].occlusion_plus_5_type_avg,
+        //     occlusion_plus_5_location_avg = responses[0].occlusion_plus_5_location_avg,
+        //     occlusion_plus_5_completely_correct_avg = responses[0].occlusion_plus_5_completely_correct_avg;
+        // clear out columns for writing to sheet
+        responses.forEach((r) => { 
+            delete r._id;
+        });
+
+        xlsx.utils.sheet_add_json(ws, responses, {header: headerKeys, skipHeader: true, origin: -1});
 
         xlsx.utils.book_append_sheet(wb, ws, 'Responses');
 
