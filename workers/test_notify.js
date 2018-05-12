34168f921afb437111fa2620032744d4e8d0a792
Paul Kohlhoff <paul.kohlhoff@gmail.com>
2018-05-12 08:36:12 -0600

    convert test_notify

--- workers/test_notify.js
+++ workers/test_notify.js
@@ -1,32 +1,12 @@
-var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
-var consumer = require('../consumer');
-var publisher = require('../publisher');
 var moment = require('moment');
 var sleep = require('sleep');
-const mongo_connectionString = require('../db').mongo_connectionString;
-const rmq_connectionString = require('../env').rmq_connectionString;
+var MongoRmqWorker = require('../lib/MongoRmqWorker');
 
 
-const q = 'test.notify';
+class Task extends MongoRmqWorker {
 
-// process.argv
-if (!process.argv.length) sleep.sleep(30);
-
-// try {
-
-    // Database Name
-  const dbName = 'prod';
-
-
-    // Use connect method to connect to the server
-  MongoClient.connect(mongo_connectionString + '/' + dbName, async function(err, client) {
-    assert.equal(null, err);
-    console.log("Connected successfully to Mongo server: " + mongo_connectionString + '/' + dbName);
-
-	    var db = client.db(dbName);
-
-		consumer.consume(async (data, msg, conn, ch) => {
+	async myTask(db, data, msg, conn, ch) {
 
 			if (data.filters && data.filters.report_viewer) {
 
@@ -36,11 +16,13 @@ if (!process.argv.length) sleep.sleep(30);
 
 				id_submissions = id_submissions.filter(s => s.id_submission != "");
 
-				publisher.publishDurable({id_submissions:id_submissions.map((s) => s.id_submission), for:'report_viewer'}, 'test.notified', rmq_connectionString);
+				this.publishDurable({id_submissions:id_submissions.map((s) => s.id_submission), for:'report_viewer'}, 'test.notified');
 			}
 
 			ch.ack(msg);
 
-		}, q, rmq_connectionString);
+	}
 
-  });
+ }
+
+ module.exports = Task;
