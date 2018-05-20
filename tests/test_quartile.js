ae1d34fbaaa6499a723b52157c35d71924220d88
Paul Kohlhoff <paul.kohlhoff@gmail.com>
2018-05-20 06:41:40 -0600

    work on quartiles

---
+++ tests/test_quartile.js
@@ -0,0 +1,114 @@
+
+var Worker = require('../workers/test_calculate_quartile');
+
+
+let Consumer = {},
+	Publisher = {},
+	Amqp = {},
+	config = {
+		database: { name: '', connectionString: '' },
+		messageBroker: { connectionString: '' }
+	},
+	db = {
+		collection: () => {
+			return {
+				find: () => {
+					return {
+						project: () => {
+							return {
+								toArray: () => {
+									return [
+										{
+											occlusion_plus_5_completely_correct_score:400,
+											occlusion_plus_2_completely_correct_score: 375,
+											occlusion_none_completely_correct_score: 700,
+											occlusion_plus_5_type_score:400,
+											occlusion_plus_2_type_score: 375,
+											occlusion_none_type_score: 700,
+											occlusion_plus_5_location_score:400,
+											occlusion_plus_2_location_score: 375,
+											occlusion_none_location_score: 700,
+											prs: 300
+										},
+										{
+											occlusion_plus_5_completely_correct_score:420,
+											occlusion_plus_2_completely_correct_score: 395,
+											occlusion_none_completely_correct_score: 720,
+											occlusion_plus_5_location_score:420,
+											occlusion_plus_2_location_score: 395,
+											occlusion_none_location_score: 720,
+											occlusion_plus_5_type_score:420,
+											occlusion_plus_2_type_score: 395,
+											occlusion_none_type_score: 720,
+											prs: 320
+										},
+										{
+											occlusion_plus_5_completely_correct_score:440,
+											occlusion_plus_2_completely_correct_score: 415,
+											occlusion_none_completely_correct_score: 740,
+											occlusion_plus_5_type_score:440,
+											occlusion_plus_2_type_score: 415,
+											occlusion_none_type_score: 740,
+											occlusion_plus_5_location_score:440,
+											occlusion_plus_2_location_score: 415,
+											occlusion_none_location_score: 740,
+											prs: 340
+										},
+										{
+											occlusion_plus_5_completely_correct_score:460,
+											occlusion_plus_2_completely_correct_score: 435,
+											occlusion_none_completely_correct_score: 760,
+											occlusion_plus_5_location_score:460,
+											occlusion_plus_2_location_score: 435,
+											occlusion_none_location_score: 760,
+											occlusion_plus_5_type_score:460,
+											occlusion_plus_2_type_score: 435,
+											occlusion_none_type_score: 760,
+											prs: 360
+										},
+										{
+											occlusion_plus_5_completely_correct_score:480,
+											occlusion_plus_2_completely_correct_score: 455,
+											occlusion_none_completely_correct_score: 780,
+											occlusion_plus_5_type_score:480,
+											occlusion_plus_2_type_score: 455,
+											occlusion_none_type_score: 780,
+											occlusion_plus_5_location_score:480,
+											occlusion_plus_2_location_score: 455,
+											occlusion_none_location_score: 780,
+											prs: 380
+										}
+									]
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	},
+	data = {
+		filter: {}
+	},
+	msg = {},
+	conn = {},
+	ch = {
+		ack: () => {}
+	}
+
+let worker = new Worker(Consumer, Publisher, Amqp, config)
+worker.myTask(db, data, msg, conn, ch)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- workers/test_calculate_quartile.js
+++ workers/test_calculate_quartile.js
@@ -19,56 +19,165 @@ class Task extends MongoRmqWorker {
   */
   async myTask(db, data, msg, conn, ch) {
 
+    try {
 
-    let result = {
-      occlusion_plus_5_completely_correct_score_q1: 0,
-      occlusion_plus_5_completely_correct_score_median: 0,
-      occlusion_plus_5_completely_correct_score_q3: 0,
+      let result = {
+        occlusion_plus_5_completely_correct_score_q1: 0,
+        occlusion_plus_5_completely_correct_score_median: 0,
+        occlusion_plus_5_completely_correct_score_q3: 0,
 
-      occlusion_plus_2_completely_correct_score_q1: 0,
-      occlusion_plus_2_completely_correct_score_median: 0,
-      occlusion_plus_2_completely_correct_score_q3: 0,
+        occlusion_plus_2_completely_correct_score_q1: 0,
+        occlusion_plus_2_completely_correct_score_median: 0,
+        occlusion_plus_2_completely_correct_score_q3: 0,
 
-      occlusion_none_completely_correct_score_q1: 0,
-      occlusion_none_completely_correct_score_median: 0,
-      occlusion_none_completely_correct_score_q3: 0,
+        occlusion_none_completely_correct_score_q1: 0,
+        occlusion_none_completely_correct_score_median: 0,
+        occlusion_none_completely_correct_score_q3: 0,
 
-      prs_q1: 0,
-      prs_median: 0,
-      prs_q3: 0,
-    };
+        occlusion_plus_5_location_score_q1: 0,
+        occlusion_plus_5_location_score_median: 0,
+        occlusion_plus_5_location_score_q3: 0,
+
+        occlusion_plus_2_location_score_q1: 0,
+        occlusion_plus_2_location_score_median: 0,
+        occlusion_plus_2_location_score_q3: 0,
+
+        occlusion_none_location_score_q1: 0,
+        occlusion_none_location_score_median: 0,
+        occlusion_none_location_score_q3: 0,
+
+        occlusion_plus_5_type_score_q1: 0,
+        occlusion_plus_5_type_score_median: 0,
+        occlusion_plus_5_type_score_q3: 0,
+
+        occlusion_plus_2_type_score_q1: 0,
+        occlusion_plus_2_type_score_median: 0,
+        occlusion_plus_2_type_score_q3: 0,
+
+        occlusion_none_type_score_q1: 0,
+        occlusion_none_type_score_median: 0,
+        occlusion_none_type_score_q3: 0,
+
+        prs_q1: 0,
+        prs_median: 0,
+        prs_q3: 0,
+
+        scoringAlgorithm: 'ScoreQuartile',
+        dateStart: data.filter.dateStart,
+        dateEnd: data.filter.dateEnd,
+        team: data.filter.team
+      };
+
+      var setQuartile = function(row, quarts, scoreAnswerKey) {
+        var rowScore = scoreAnswerKey != 'prs' ? row[`occlusion_${scoreAnswerKey}_score`] : row.prs;
+            if (rowScore > quarts[0]) {
+
+              if (rowScore > quarts[1]) {
+                if (rowScore > quarts[2]) {
+                  row[`${scoreAnswerKey}_quartile`] = quartileNames[3];
+                } else {
+                  row[`${scoreAnswerKey}_quartile`] = quartileNames[2];
+                }
+              } else {
+                row[`${scoreAnswerKey}_quartile`] = quartileNames[1];
+              }
+            } else {
+              row[`${scoreAnswerKey}_quartile`] = quartileNames[0];
+            }
+      };
 
 
-    let query = {
-      time: {$gte: data.filter.dateStart},
-      time: {$lte: data.filter.dateEnd},
-      team: data.filter.team,
-    },
-    projection = {
-      occlusion_plus_5_completely_correct_score: 1,
-      occlusion_plus_2_completely_correct_score: 1,
-      occlusion_none_completely_correct_score: 1,
-      prs: 1
-    };
+      let query = {
+        team: data.filter.team,
+        scoringAlgorithm: "AvgToPercent"
+      },
+      projection = {
+        id_submission: 1,
+        occlusion_plus_5_completely_correct_score: 1,
+        occlusion_plus_2_completely_correct_score: 1,
+        occlusion_none_completely_correct_score: 1,
+        occlusion_plus_5_type_score: 1,
+        occlusion_plus_2_type_score: 1,
+        occlusion_none_type_score: 1,
+        occlusion_plus_5_location_score: 1,
+        occlusion_plus_2_location_score: 1,
+        occlusion_none_location_score: 1,
+        prs: 1
+      };
 
-    let rows = await db.collection(c).find(query).project(projection).toArray();
+      if (data.filter.dateStart) {
+        Object.assign(query.test_date, {$gte: data.filter.dateStart});
+      }
+      if (data.filter.dateEnd) {
+        Object.assign(query.test_date, {$lte: data.filter.dateEnd});
+      }
 
-    let maps = {
-      plus_5: rows.map(x=>x.occlusion_plus_5_completely_correct_score),
-      plus_2: rows.map(x=>x.occlusion_plus_2_completely_correct_score),
-      none: rows.map(x=>x.occlusion_none_completely_correct_score),
-      prs: rows.map(x=>x.prs)
-    };
+      let rows = await db.collection(c).find(query).project(projection).toArray();
 
-    for (let scoreType in ['plus_5', 'plus_2', 'none', 'prs']) {
-      let quarts = jStat.quartiles(maps[scoreType]);
-      results['occlusion_plus_5_completely_correct_score_q1'] = quarts[0];
-      results['occlusion_plus_5_completely_correct_score_q1'] = quarts[1];
-      results['occlusion_plus_5_completely_correct_score_q1'] = quarts[2];
+      let answerTypes = ['completely_correct', 'type', 'location'];
+
+      for (let answerType of answerTypes) {
+
+        let maps = {
+          plus_5: rows.map(x=>x['occlusion_plus_5_'+answerType+'_score']),
+          plus_2: rows.map(x=>x['occlusion_plus_2_'+answerType+'_score']),
+          none: rows.map(x=>x['occlusion_none_'+answerType+'_score'])
+        };
+
+        for (let scoreType of ['plus_5', 'plus_2', 'none']) {
+          let quarts = jStat.quartiles(maps[scoreType]);
+          result[`occlusion_${scoreType}_${answerType}_score_q1`] = quarts[0];
+          result[`occlusion_${scoreType}_${answerType}_score_median`] = quarts[1];
+          result[`occlusion_${scoreType}_${answerType}_score_q3`] = quarts[2];
+
+          rows.forEach(row => {
+            setQuartile(row, quarts, `${scoreType}_${answerType}`);
+          });
+        }
+      }
+
+      let quarts = jStat.quartiles(rows.map(x=>x.prs));
+      result.prs_q1 = quarts[0];
+      result.prs_median = quarts[1];
+      result.prs_q3 = quarts[2];
+
+      rows.forEach(row => {
+        setQuartile(row, quarts, 'prs');
+      });
+
+      console.log(result);
+      console.log(rows);
+
+      rows = rows.map(row => {
+        return {
+          id_submission: row.id_submission,
+          quartile_filter: data.filter,
+          plus_5_completely_correct_quartile: row.plus_5_completely_correct_quartile,
+          plus_2_completely_correct_quartile: row.plus_2_completely_correct_quartile,
+          none_completely_correct_quartile: row.none_completely_correct_quartile,
+          plus_5_type_quartile: row.plus_5_type_quartile,
+          plus_2_type_quartile: row.plus_2_type_quartile,
+          none_type_quartile: row.none_type_quartile,
+          plus_5_location_quartile: row.plus_5_location_quartile,
+          plus_2_location_quartile: row.plus_2_location_quartile,
+          none_location_quartile: row.none_location_quartile,
+          prs_quartile: row.prs_quartile
+        }
+      });
+
+      console.log(rows);
+
+      await Promise.all(
+        [db.collection('test_calc').update({dateStart:result.dateStart, dateEnd:result.dateEnd, team:result.team}, result, {upsert:true})]
+        .concat(rows.map(row => db.collection('test_usage').updateMany({id_submission:row.id_submission}, {$set:row})))
+      );
+
+      ch.ack(msg);
+
+    } catch (ex) {
+      console.log("Error: " + (ex.stack ? ex : ""));
+      console.error(ex.stack || ex);
     }
-
-    console.info(result);
-
   }
 }
 
