1bfa16bf2b55ca93c5e9934b3d89aae3eb525935
pablit07 <paul.kohlhoff@gmail.com>
2018-06-27 08:23:54 -0600

    Update sample_1.js

--- workers/sample_1.js
+++ workers/sample_1.js
@@ -1,20 +1,6 @@
-var assert = require('assert');
-var moment = require('moment');
-const uuid = require('uuid/v4');
-var sleep = require('sleep');
-var MongoRmqWorker = require('../lib/MongoRmqWorker');
-
-// calc single player scores
-
-const q_pub = 'test.notify';
-const c = 'test_calc';
-
-
 class Task extends MongoRmqWorker {
 
-  /*
-     calc single player scores
-  */
+  /* run a single unit of work */
   async myTask(db, msgContent, msg, conn, ch) {
 
     let promise = new Promise(resolve => {
