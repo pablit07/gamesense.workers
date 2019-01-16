ac85b584ddda3dd21d1c2cd42122125ccfa1b059
Paul Kohlhoff <paul.kohlhoff@gmail.com>
2019-01-16 12:06:25 -0600

    drill usage summary

---
+++ tests/drill_usageSummary.js
@@ -0,0 +1,150 @@
+var validate = require('jsonschema').validate;
+var Worker = require('../workers/drill_usageSummary');
+
+
+
+let rows = [ {"id":"81006f3b-ede4-4089-8112-72f3981f8375","occlusion_plus_5_type_score":0,"occlusion_plus_5_type_avg":0,"occlusion_plus_2_type_score":0,"occlusion_plus_2_type_avg":0,"occlusion_none_type_score":0,"occlusion_none_type_avg":0,"occlusion_plus_5_location_score":0,"occlusion_plus_5_location_avg":0,"occlusion_plus_2_location_score":0,"occlusion_plus_2_location_avg":0,"occlusion_none_location_score":0,"occlusion_none_location_avg":0,"occlusion_plus_5_completely_correct_score":0,"occlusion_plus_5_completely_correct_avg":0,"occlusion_plus_2_completely_correct_score":0,"occlusion_plus_2_completely_correct_avg":0,"occlusion_none_completely_correct_score":0,"occlusion_none_completely_correct_avg":0,"occlusion_plus_2_plus_5_type_avg":0,"occlusion_plus_2_plus_5_location_avg":0,"occlusion_plus_2_plus_5_completely_correct_avg":0,"total_type_score":0,"total_location_score":0,"total_completely_correct_score":0,"totalRowCount":1,"prs":null,"processed_worker":"2019-01-14T12:22:36-07:00","scoringAlgorithm":"AvgToPercent","rowCount_plus_2":1,"rowCount_plus_5":1,"rowCount_none":1,"team":"","team_name":"","team_id":"","user_id":1159,"player_id":"1159 dean eng","drill_date":"January 11th 2019","drill_date_raw":"2019-01-11T07:00:00.000Z","player_batting_hand":"R","drill":"Michaela (LHP v RHB) - Basic","pitcher_name":"Michaela","difficulty":"Basic"}];
+
+
+let Consumer = {},
+	Publisher = {},
+	Amqp = {},
+	config = {
+		database: { name: '', connectionString: '' },
+		messageBroker: { connectionString: '' }
+	},
+	db = {
+		collection: () => {
+			return {
+				find: () => {
+          return {
+            project: () => {
+  					 return {
+  					   	toArray: () => {
+  						  	return rows;
+  						  }
+              }
+  					}
+          }
+				},
+				update: () => {},
+				updateMany: () => {},
+				count: () => { return 1; }
+			}
+		}
+	},
+	data = {
+		filter: {}
+	},
+	msg = {},
+	conn = {},
+	ch = {
+		ack: () => {}
+	}
+
+let worker = new Worker(Consumer, Publisher, Amqp, config);
+
+var doIt = async function() {
+	var response = await worker.myTask(data, msg, conn, ch, db);
+
+	console.log(validate(worker.getSchema(), response).errors == 0);
+};
+
+doIt();
+
+
+
+/*
+Expected:
+
+[ { number_of_responses: 72,
+       test_date: '2017-08-25T21:02:56.000Z',
+       id_submission: 'f16cf0d1-72f4-4b53-af6c-a7fe26b4ef13',
+       source_etl: 'GameSenseSports2018-07-07-20-41',
+       team: 'Reds',
+       player_id: '650441 Leandro Santana' },
+     { number_of_responses: 72,
+       test_date: '2017-04-27T16:55:21.000Z',
+       id_submission: 'fef86479-910a-488a-9ef8-c1b1b3544916',
+       source_etl: 'GameSenseSportsminors22017-04-27-18-00',
+       team: 'Southern Illinois Miners',
+       player_id: '9 Zaire Kutsulis' },
+     { number_of_responses: 72,
+       test_date: '2018-03-01T21:55:33.000Z',
+       id_submission: '654cc06a-5cfb-47f5-bc94-7d5ad3cdfab4',
+       source_etl: 'GameSenseSports2018-07-07-20-41',
+       team: 'Reds',
+       player_id: '642689 Francis Ancona' },
+     { number_of_responses: 72,
+       test_date: '2018-07-06T22:10:31.000Z',
+       id_submission: 'aa91c8eb-b01a-4705-afda-4dcdf776e5cf',
+       source_etl: 'GameSenseSports2018-07-07-20-41',
+       team: 'Reds',
+       player_id: 'dr18008 junior  tamares ' },
+     { number_of_responses: 72,
+       test_date: '2018-07-06T21:51:47.000Z',
+       id_submission: '1f567aeb-5989-450e-8fa2-b2587f1f89f1',
+       source_etl: 'GameSenseSports2018-07-07-20-41',
+       team: 'Reds',
+       player_id: 'DR18006 Wilmer Alcantara' },
+     { number_of_responses: 72,
+       test_date: '2017-04-18T17:20:54.000Z',
+       id_submission: '2f578bc8-d2a2-43a6-bac1-dcc4bf592a9e',
+       source_etl: 'GameSenseSports REDS CL 2espanol2017-08-18-18-08',
+       team: 'Reds',
+       player_id: '673656 Deybert Lozano' },
+     { number_of_responses: 72,
+       test_date: '2018-07-06T20:49:40.000Z',
+       id_submission: '3cac867b-249b-4ef7-aa0c-056b610627eb',
+       source_etl: 'GameSenseSports2018-07-07-20-41',
+       team: 'Reds',
+       player_id: 'DR18003 Luis Cabrera' },
+     { number_of_responses: 72,
+       test_date: '2018-08-21T19:56:57.000Z',
+       id_submission: '06783d7d-a18f-445d-a183-c0bc1cb68e9a',
+       source_etl: 'GameSenseSports2018-08-27-18-40',
+       team: 'Reds',
+       player_id: '602115 Gabriel  Guerrero ' },
+     { number_of_responses: 72,
+       test_date: '2018-07-07T19:23:38.000Z',
+       id_submission: '562b7d82-ca1c-433d-8675-c760ea23c1a4',
+       source_etl: 'GameSenseSports2018-07-07-20-41',
+       team: 'Reds',
+       player_id: 'Do219 003 Cesar Julio Martinez' },
+     { number_of_responses: 72,
+       test_date: '2018-04-11T16:56:39.000Z',
+       id_submission: '999cbfaf-9750-46b4-8695-1eebf337e5b3',
+       source_etl: 'GameSenseSports2018-07-07-20-41',
+       team: 'Reds',
+       player_id: '678259 Emilio Garcia' },
+     { number_of_responses: 72,
+       test_date: '2018-02-20T15:47:08.000Z',
+       id_submission: '50bb78dc-6723-49a1-9e93-c4899dd1d9f8',
+       source_etl: '2018-02-20-16-08',
+       player_id: '01 test peter' },
+     { number_of_responses: 72,
+       test_date: '2018-03-03T18:23:27.000Z',
+       id_submission: '82ffc908-0f3a-4c49-a0bd-962ea802d3b3',
+       source_etl: 'GameSenseSports2018-07-06-22-32',
+       team: 'Reds',
+       player_id: '660647 Mariel Bautista' },
+     { number_of_responses: 72,
+       test_date: '2018-07-06T21:25:51.000Z',
+       id_submission: '222ada1d-ee3d-4410-9b49-22842c882947',
+       source_etl: 'GameSenseSports2018-07-07-20-41',
+       team: 'Reds',
+       player_id: 'Dr18004 Elly Antonio  De La Cruz Sanchez ' },
+     { number_of_responses: 72,
+       test_date: '2018-04-12T16:46:20.000Z',
+       id_submission: '15dafaa5-9357-40af-b3b8-39b81b9305e5',
+       source_etl: 'GameSenseSports2018-07-07-20-41',
+       team: 'Reds',
+       player_id: 'DRTamares junior tamares ' },
+     { number_of_responses: 72,
+       test_date: '2017-08-25T20:49:36.000Z',
+       id_submission: '174da437-e8be-448e-83a8-53ea36fc9826',
+       source_etl: 'GameSenseSports2018-07-07-20-41',
+       team: 'Reds',
+       player_id: '642298 panel man Zander of' } ]
+
+*/
--- workers/drill_usageSummary.js
+++ workers/drill_usageSummary.js
@@ -2,12 +2,12 @@ var MongoRmqApiWorker = require('../lib/MongoRmqApiWorker');
 var moment = require('moment');
 var schemas = require('../schemas');
 
-const c = 'test_calc';
+const c = 'drill_calc';
 
 class Task extends MongoRmqApiWorker {
 
 	getSchema() {
-		return schemas.test_usageSummary;
+		return schemas.drill_usageSummary;
 	}
 
   	/*
@@ -15,25 +15,13 @@ class Task extends MongoRmqApiWorker {
 	*/
 	async myTask(data, msg, conn, ch, db) {
 
-		var rows = await db.collection('test_usage').aggregate([{ 
-			$group:{
-				_id:{
-					'id_submission':'$id_submission',
-					'source_etl':'$source_etl',
-					'team':'$team',
-					'app':'$app',
-					'player_id':'$player_id',
-				},
-				test_date:{$first:'$time_video_started'},
-				number_of_responses:{$sum:1}
-			},
-		}, {$sort:{"test_date":-1} }]).toArray();
+		var rows = await db.collection(c).find({}, {$sort:{"completion_timestamp":-1} }).project({id_submission:1,team_name:1,player_first_name:1,player_last_name:1,drill:1,first_glance_total_score:1,app:1,completion_timestamp_formatted:1}).toArray();
 
 		console.log(` [x] Wrote ${JSON.stringify(rows)} to ${this.DbName + '.' + c}`);
 
 		ch.ack(msg);
 
-		return rows.map(x => Object.assign({number_of_responses: x.number_of_responses, test_date: (x.test_date ? moment(x.test_date).utcOffset(-6).format('MMMM Do YYYY') : null)}, x._id));
+		return rows;
 	}
 
 
