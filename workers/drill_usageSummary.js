ae3b21aaa30c9365f44e2457b371469e38fdfd2c
Paul Kohlhoff <paul.kohlhoff@gmail.com>
2019-01-19 09:08:45 -0700

    workers/test_usageSummary.js

--- workers/drill_usageSummary.js
+++ workers/drill_usageSummary.js
@@ -1,8 +1,8 @@
-var MongoRmqApiWorker = require('../lib/MongoRmqApiWorker');
-var moment = require('moment');
-var schemas = require('../schemas');
+var MongoRmqApiWorker = require("../lib/MongoRmqApiWorker");
+var moment = require("moment");
+var schemas = require("../schemas");
 
-const c = 'drill_calc';
+const c = "drill_calc";
 
 class Task extends MongoRmqApiWorker {
 
@@ -15,9 +15,13 @@ class Task extends MongoRmqApiWorker {
 	*/
 	async myTask(data, msg, conn, ch, db) {
 
-		var rows = await db.collection(c).find({}, {$sort:{"completion_timestamp":-1} }).project({id_submission:1,team_name:1,player_first_name:1,player_last_name:1,drill:1,first_glance_total_score:1,app:1,completion_timestamp_formatted:1}).toArray();
+		var rows = await db.collection(c).find({}, {$sort:{"completion_timestamp":-1} }).project({id_submission:1,team_name:1,player_first_name:1,player_last_name:1,drill:1,app:1,first_glance_total_score:1,completion_timestamp_formatted:1}).toArray();
 
-		console.log(` [x] Wrote ${JSON.stringify(rows)} to ${this.DbName + '.' + c}`);
+		rows = rows.map(r => {delete r._id; return Object.assign({
+			app: r.app || "",
+			first_glance_total_score: r.first_glance_total_score || 0
+		}, r);});
+		console.log(` [x] Wrote ${JSON.stringify(rows)} to ${this.DbName + "." + c}`);
 
 		ch.ack(msg);
 
