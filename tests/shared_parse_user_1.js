5b2640a730f1cd2505e73e99470e31bdd1eb1ab6
Paul Kohlhoff <paul.kohlhoff@gmail.com>
2020-05-27 07:51:19 -0400

    add user parse test and rename parse activity

---
+++ tests/shared_parse_user_1.js
@@ -0,0 +1,78 @@
+
+var Worker = require('../workers/shared_parse_user');
+
+
+let dataRow = {"id":24237,"first_name":"Paul","last_name":"Kohlhoff","team":"","email":"paul.kohlhoff+test71367@gmail.com","username":"paul.kohlhoff+test71367@gmail.com","app":"BB"}
+
+
+
+
+let Consumer = {},
+	Publisher = {},
+	Amqp = {},
+	config = {
+		database: { name: '', connectionString: '' },
+		messageBroker: { connectionString: '' },
+		rollbar: {token: ''}
+	},
+	db = {
+		collection: () => {
+			return {
+				update: () => {},
+				updateMany: () => {},
+				count: () => { return 1; },
+				findOneAndUpdate: () => {}, 
+				find: () => {
+					return {
+						project: () => {
+							return {
+								toArray: () => {
+									return [
+										dataRow
+									]
+								}
+							}
+						},
+						toArray: () => {
+							return [
+								dataRow
+							]
+						},
+						limit: () => {
+							return {
+								toArray: () => {
+									return [
+										dataRow
+									]
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	},
+	data = dataRow,
+	msg = {},
+	conn = {},
+	ch = {
+		ack: () => {}
+	}
+
+let worker = new Worker(Consumer, Publisher, Amqp, config)
+worker.myTask(data, msg, conn, ch, db)
+
+
+
+
+
+
+ // input {"id":24237,"first_name":"Paul","last_name":"Kohlhoff","team":"","email":"paul.kohlhoff+test71367@gmail.com","username":"paul.kohlhoff+test71367@gmail.com","app":"BB"}
+ // expected: {"app":"BB","id":24237,"first_name":"Paul","last_name":"Kohlhoff","team":"","email":"paul.kohlhoff+test71367@gmail.com","username":"paul.kohlhoff+test71367@gmail.com","processed_worker":"2020-05-27T11:46:45+00:00","id_worker":"8bb51dbf-40fa-4770-a3f9-c21ef05b3558"} to prod.users
+
+
+
+
+
+
+
