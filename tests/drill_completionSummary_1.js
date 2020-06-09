15d020a1b74a816df40ea91a3c2b9b8a98f69bea
MackHalliday <mackenzie.halliday@gmail.com>
2020-06-09 17:46:34 -0600

    Update testing to include player_first_name and player_last_name

--- tests/drill_completionSummary_1.js
+++ tests/drill_completionSummary_1.js
@@ -2,24 +2,24 @@ var validate = require('jsonschema').validate;
 var Worker = require('../workers/drill_completionSummary');
 
 
-let rows = [ { _id: { user_id: 2, month: 4, year: 2020 }, count: 21 },
-	{ _id: { user_id: 3, month: 4, year: 2020 }, count: 7 },
-	{ _id: { user_id: 3, month: 5, year: 2020 }, count: 6 },
-	{ _id: { user_id: 152, month: 4, year: 2020 }, count: 1 },
-	{ _id: { user_id: 152, month: 5, year: 2020 }, count: 2 },
-	{ _id: { user_id: 1088, month: 5, year: 2020 }, count: 3 },
-	{ _id: { user_id: 12061, month: 4, year: 2020 }, count: 2 },
-	{ _id: { user_id: 16040, month: 4, year: 2020 }, count: 2 },
-	{ _id: { user_id: 23449, month: 5, year: 2020 }, count: 2 } ];
-let cachedRows = [ { _id: { user_id: 2, month: 4, year: 2020 }, count: 21 },
-	{ _id: { user_id: 3, month: 4, year: 2020 }, count: 7 },
-	{ _id: { user_id: 3, month: 5, year: 2020 }, count: 6 },
-	{ _id: { user_id: 152, month: 4, year: 2020 }, count: 1 },
-	{ _id: { user_id: 152, month: 5, year: 2020 }, count: 2 },
-	{ _id: { user_id: 1088, month: 5, year: 2020 }, count: 3 },
-	{ _id: { user_id: 12061, month: 4, year: 2020 }, count: 2 },
-	{ _id: { user_id: 16040, month: 4, year: 2020 }, count: 2 },
-	{ _id: { user_id: 23449, month: 5, year: 2020 }, count: 2 } ];
+let rows = [ { _id: { user_id: 2, month: 4, year: 2020,  play_first_name: "First", player_last_name: "Last"}, count: 21 },
+	{ _id: { user_id: 3, month: 4, year: 2020, player_first_name: "First", player_last_name: "Last" }, count: 7 },
+	{ _id: { user_id: 3, month: 5, year: 2020, player_first_name: "First", player_last_name: "Last"}, count: 6 },
+	{ _id: { user_id: 152, month: 4, year: 2020, player_first_name: "First", player_last_name: "Last"}, count: 1 },
+	{ _id: { user_id: 152, month: 5, year: 2020, player_first_name: "First", player_last_name: "Last"}, count: 2 },
+	{ _id: { user_id: 1088, month: 5, year: 2020, player_first_name: "First", player_last_name: "Last"}, count: 3 },
+	{ _id: { user_id: 12061, month: 4, year: 2020, player_first_name: "First", player_last_name: "Last"}, count: 2 },
+	{ _id: { user_id: 16040, month: 4, year: 2020, player_first_name: "First", player_last_name: "Last"}, count: 2 },
+	{ _id: { user_id: 23449, month: 5, year: 2020, player_first_name: "First", player_last_name: "Last"}, count: 2 } ];
+let cachedRows = [ { _id: { user_id: 2, month: 4, year: 2020, player_first_name: "First", player_last_name: "Last" }, count: 21 },
+	{ _id: { user_id: 3, month: 4, year: 2020, player_first_name: "First", player_last_name: "Last" }, count: 7 },
+	{ _id: { user_id: 3, month: 5, year: 2020, player_first_name: "First", player_last_name: "Last" }, count: 6 },
+	{ _id: { user_id: 152, month: 4, year: 2020, player_first_name: "First", player_last_name: "Last" }, count: 1 },
+	{ _id: { user_id: 152, month: 5, year: 2020, player_first_name: "First", player_last_name: "Last" }, count: 2 },
+	{ _id: { user_id: 1088, month: 5, year: 2020, player_first_name: "First", player_last_name: "Last" }, count: 3 },
+	{ _id: { user_id: 12061, month: 4, year: 2020, player_first_name: "First", player_last_name: "Last" }, count: 2 },
+	{ _id: { user_id: 16040, month: 4, year: 2020, player_first_name: "First", player_last_name: "Last" }, count: 2 },
+	{ _id: { user_id: 23449, month: 5, year: 2020, player_first_name: "First", player_last_name: "Last" }, count: 2 } ];
 
 let Consumer = {},
 	Publisher = {},
@@ -82,5 +82,3 @@ var doIt = async function() {
 };
 
 doIt();
-
-
