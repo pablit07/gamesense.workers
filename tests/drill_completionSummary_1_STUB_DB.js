4d6e36d0b91242c4fbd120ef8ef99b6d724d0a2d
MackHalliday <mackenzie.halliday@gmail.com>
2020-06-20 10:49:03 -0600

    Update correct file with new testing setup for stubbed test database

--- tests/drill_completionSummary_1_STUB_DB.js
+++ tests/drill_completionSummary_1_STUB_DB.js
@@ -1,67 +1,62 @@
 const MongoClient = require('mongodb').MongoClient
-var validate = require('jsonschema').validate;
-var Worker = require('../workers/drill_completionSummary');
-var pry = require('pryjs')
-
+const EJSON = require('ejson')
 const mongoUnit = require('mongo-unit')
-const testData = require('./fixtures/drill_comp.json')
+let validate = require('jsonschema').validate
+let Worker = require('../workers/drill_completionSummary')
+let pry = require('pryjs')
+let assert = require('assert')
 
-// const Fixtures = require('node-mongodb-fixtures');
-// const fixtures = new Fixtures({
-//   dir:'tests/fixtures/',
-//   mute: false, // do not mute the log output
-// });
+async function testWorkerResponse() {
 
-// const testData = require('./fixtures/drill_comp.json')
- async function myFunction() {
-
-	await mongoUnit.start()
+  // create test database
+  await mongoUnit.start()
 	const connectionUrl = mongoUnit.getUrl()
   let client = await MongoClient.connect(connectionUrl);
   let db = await client.db('test')
-  await mongoUnit.load(testData)
-	// await fixtures.connect(connectionUrl)
-	// await fixtures.unload()
-  // await fixtures.load()
-	// await fixtures.disconnect()
 
+  // load test data
+  const testData = require('./fixtures/drill_comp.json')
+  await mongoUnit.load(EJSON.fromJSONValue(testData))
 
-	let Consumer = {},
-		Publisher = {},
-		Amqp = {},
-		config = {
+	let Consumer = {}
+	let Publisher = {}
+	let Amqp = {}
+	let config = {
 			database: { name: '', connectionString: '' },
 			messageBroker: { connectionString: '' },
 			rollbar: { token: '' }
-		},
-		data = {
+		}
+
+  // setup data filters
+	let data = {
 			filters: {},
-			authToken: {id:1,app:'BB'},
+			authToken: {id:150, app:'BB'},
 			rollUpType: 'monthly'
-		},
-		msg = {},
-		conn = {},
-		ch = {
+		}
+	let msg = {}
+	let conn = {}
+	let ch = {
 			ack: () => {}
 		}
 
-	let count = await db.collection('drill_comp').countDocuments({})
 	let worker = await new Worker(Consumer, Publisher, Amqp, config);
-	// var doIt = async function() {
-  //
-	// 	var response = await worker.myTask(data, msg, conn, ch, db);
-	// 	console.log("helloo" response)
-	// 	// console.log(validate(worker.getSchema(), response).errors === 0);
-	// };
-  //
-	// doIt();
-
   let response = await worker.myTask(data, msg, conn, ch, db);
 
-  // console.log("response data: " + response)
-    // console.log(validate(worker.getSchema(), response).errors === 0);
+  console.log("response data: " + JSON.stringify(response))
+
+  // basic assert statements for testing
+  // throws errror if statement false
+  await assert(response[0].count === 2);
+  await assert(response[0].user_id === 150);
+  await assert(response[0].month === 5);
+  await assert(response[0].year === 2019);
+  await assert(response[0].player_first_name === 'Coach');
+  await assert(response[0].player_last_name === 'Kohlhoff');
+  await assert(response[0].date.toISOString() === "2019-04-30T06:00:00.000Z");
+  await assert(response[0].date_format === "5-2019");
+
   await mongoUnit.stop()
-	return console.log("response: " + response)
+  return response
 }
 
-myFunction();
+testWorkerResponse();
